7364e92206b4e033e3e34ef2d772d5b72e593bbf
Robert Winkler <robert.winkler@wp.pl>
2012-04-13 23:16:55 +0200

    kod

---
+++ DynamicUserInterfaceExample/Common/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DynamicUserInterfaceExample.Common
+{
+    public class RelayCommand : ICommand
+    {
+        #region Fields
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+        #endregion
+
+        #region Constructors
+        public RelayCommand(Action execute) : this(k => execute()) { }
+
+        public RelayCommand(Action<object> execute)
+            : this(execute, null) { }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null) throw new ArgumentNullException("execute");
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        #endregion
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+        #endregion
+    }
+}
---
+++ DynamicUserInterfaceExample/ViewModels/MainVindowViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using DynamicUserInterfaceExample.Common;
+
+namespace DynamicUserInterfaceExample.ViewModels
+{
+    public class MainVindowViewModel : INotifyPropertyChanged
+    {
+        private object _leftPanel = new FirstLeftViewModel();
+        public object LeftPanel
+        {
+            get { return _leftPanel; }
+            set
+            {
+                _leftPanel = value;
+                TriggerPropertyChangedEvent("LeftPanel");
+            }
+        }
+
+        private object _rightPanel = new FirstRightViewModel();
+        public object RightPanel
+        {
+            get { return _rightPanel; }
+            set
+            {
+                _rightPanel = value;
+                TriggerPropertyChangedEvent("RightPanel");
+            }
+        }
+
+        ICommand _firstOptionCommand = null;
+        public ICommand FirstOptionCommand
+        {
+            get
+            {
+                if (_firstOptionCommand == null)
+                    _firstOptionCommand = new RelayCommand(OnFirstOption);
+                return _firstOptionCommand;
+            }
+        }
+
+        public void OnFirstOption()
+        {
+            this.LeftPanel = new FirstLeftViewModel();
+            this.RightPanel = new FirstRightViewModel();
+        }
+
+        ICommand _secondOptionCommand = null;
+        public ICommand SecondOptionCommand
+        {
+            get
+            {
+                if (_secondOptionCommand == null)
+                    _secondOptionCommand = new RelayCommand(OnSecondOption);
+                return _secondOptionCommand;
+            }
+        }
+
+        public void OnSecondOption()
+        {
+            this.LeftPanel = new SecondLeftViewModel();
+            this.RightPanel = new SecondRightViewModel();
+        }
+
+        ICommand _thirdOptionCommand = null;
+        public ICommand ThirdOptionCommand
+        {
+            get
+            {
+                if (_thirdOptionCommand == null)
+                    _thirdOptionCommand = new RelayCommand(OnThirdOption);
+                return _thirdOptionCommand;
+            }
+        }
+
+        public void OnThirdOption()
+        {
+            this.LeftPanel = new ThirdLeftViewModel();
+            this.RightPanel = new ThirdRightViewModel();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void TriggerPropertyChangedEvent(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
